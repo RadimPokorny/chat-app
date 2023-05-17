477c400aef8f59af355ab69e3c5573955ddc1dda
RadimPokorny19 <98946296+RadimPokorny@users.noreply.github.com>
2023-05-17 14:52:44 +0200

    ultra update new final style, some scripts, responsivity of contacts added
    not so goofy, but still it is

--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
 function getSubString() {
     var message = "Hello, stranger, I would like to tell you something, but don´t know how.";
   
-    var shortMessage = message.substring(0, 23);
+    var shortMessage = message.substring(0, 35);
     var elements = document.querySelectorAll("#last-message");
   
     elements.forEach(function(element) {
@@ -9,14 +9,7 @@ function getSubString() {
     });
   }
   
-  window.onload = getSubString;
+window.onload = getSubString;
 
-  function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("chat-here").innerHTML = this.responseText;
-      }
-    xhttp.open("GET", "index.html", true);
-    xhttp.send();
-}
+
   
