8634a22d46616050cb18cd12c2da54b679131603
RadimPokorny19 <98946296+RadimPokorny@users.noreply.github.com>
2023-05-19 08:55:08 +0200

    contacts header fixed
    gfy

--- contacts.js
+++ contacts.js
@@ -1,6 +1,8 @@
 function getSubString() {
     var message = "Hello, stranger, I would like to tell you something, but don´t know how.";
   
+    
+
     var shortMessage = message.substring(0, 35);
     var elements = document.querySelectorAll("#last-message");
   
